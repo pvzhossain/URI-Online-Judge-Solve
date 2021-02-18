c22a985fa341d9510bc836d9eac3d3fa8bb0d722
Parvez Hossain <pvzhossain@gmail.com>
2021-02-18 11:52:24 +0600

    Create problem_1064.c

---
+++ problem_1064.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    float n,avg,sum=0.0;
+    int count=0, i;
+
+    for(i=0; i<6; i++)
+    {
+        scanf("%f", &n);
+        if(n>0)
+        {
+            sum+=n;
+            count++;
+        }
+
+        avg=sum/count;
+    }
+
+
+    printf("%d valores positivos\n", count);
+    printf("%.1f\n", avg);
+
+    return 0;
+}
