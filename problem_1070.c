a91d31ba5f17305da79fec3212af08f2547caec3
Parvez Hossain <pvzhossain@gmail.com>
2021-02-18 11:54:15 +0600

    Create problem_1070.c

---
+++ problem_1070.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+    int n,i;
+    scanf("%d", &n);
+    if(n%2==0)
+    {
+        n=n+1;
+    }
+    for(i=n; i<=n+10; i=i+2)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
