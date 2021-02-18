6dde5593d43fd8789486cd0ed8e118a40b24ebae
Parvez Hossain <pvzhossain@gmail.com>
2021-02-18 11:53:17 +0600

    Create problem_1066.c

---
+++ problem_1066.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c1=0,c2=0,c3=0,c4=0;
+
+    for(i=1; i<=5; i++)
+    {
+        scanf("%d", &n);
+
+        if(n%2==0)
+        {
+            c1++;
+        }
+        if(n%2!=0)
+        {
+            c2++;
+        }
+        if(n>0)
+        {
+            c3++;
+        }
+        if(n<0)
+        {
+            c4++;
+        }
+    }
+    printf("%d valor(es) par(es)\n", c1);
+    printf("%d valor(es) impar(es)\n", c2);
+    printf("%d valor(es) positivo(s)\n", c3);
+    printf("%d valor(es) negativo(s)\n", c4);
+
+    return 0;
+}
