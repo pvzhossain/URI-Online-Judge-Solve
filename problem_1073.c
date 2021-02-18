ac9f7d5e75fd2d399aacea208d3adbe8bf889a5e
Parvez Hossain <pvzhossain@gmail.com>
2021-02-18 11:55:47 +0600

    Create problem_1073.c

---
+++ problem_1073.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        if(i%2==0)
+        {
+            x=i*i;
+            printf("%d^2 = %d\n", i, x);
+        }
+    }
+
+    return 0;
+}
