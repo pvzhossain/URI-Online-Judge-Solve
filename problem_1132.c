4b353d0dee1dfb5640f008d649263d931f85eced
Parvez Hossain <pvzhossain@gmail.com>
2021-02-18 12:05:13 +0600

    Create problem_1132.c

---
+++ problem_1132.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, i, temp, sum=0;
+    scanf("%d %d", &x, &y);
+    if(x>y)
+    {
+        temp = y;
+        y = x;
+        x = temp;
+    }
+    for(i=x; i<=y; i++)
+    {
+        if(i%13!=0)
+        sum = sum + i;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
